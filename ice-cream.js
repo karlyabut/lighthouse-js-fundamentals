02b42a49d74ccc5039c832642466af5cdb7859e5
karlyabut <karleisenyabut@gmail.com>
2019-07-25 21:08:02 +0000

    Ice cream array challenge

---
+++ ice-cream.js
@@ -0,0 +1,16 @@
+// Do not modify these first two lines
+const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+
+// Your code below here...
+iceCreamFlavours.push("root beer");
+console.log(iceCreamFlavours);
+
+//get "chocolate" from array
+console.log(iceCreamFlavours[0]);
+
+//get "root beer" from array
+console.log(iceCreamFlavours[5]);
+
+//get the number of elements in array
+console.log(iceCreamFlavours.length);
